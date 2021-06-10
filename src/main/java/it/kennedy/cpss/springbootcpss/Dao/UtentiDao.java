fa5aa05da46b7008e87ba21799f1b0da2abf5290
SolivoM <matteo.solivo@tecnicosuperiorekennedy.it>
2021-06-10 12:05:29 +0200

    add UserDao e User Dto

--- src/main/java/it/kennedy/cpss/springbootcpss/Dao/UtentiDao.java
+++ src/main/java/it/kennedy/cpss/springbootcpss/Dao/UtentiDao.java
@@ -1,5 +1,28 @@
 package it.kennedy.cpss.springbootcpss.Dao;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Table (name = "TUsers")
+@Entity
+@Data
 public class UtentiDao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (name = "UserID")
+	private int UserID;
+	
+	@Column (name = "Username")
+	private String Username;
+	
+	@Column (name = "Password")
+	private String Password;
+	
 }
--- src/main/java/it/kennedy/cpss/springbootcpss/Dto/UtentiDto.java
+++ src/main/java/it/kennedy/cpss/springbootcpss/Dto/UtentiDto.java
@@ -1,5 +1,15 @@
 package it.kennedy.cpss.springbootcpss.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
 public class UtentiDto {
 
+	private Integer UserID;
+	private String Username;
+	private String Password;
 }
